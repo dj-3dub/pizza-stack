a3ccc284080a1312eb58a35733d9f73e26c23a27
Tim Heverin <192537491+dj-3dub@users.noreplygithub.com>
2025-09-02 20:13:13 -0500

    Repo hygiene: untrack vendor aws/ and old iac-localstack/, add .gitignore

--- iac-localstack/scripts/awslocal.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-AWS_REGION="${AWS_REGION:-us-east-1}"
-aws --endpoint-url=http://localhost:4566 --region "$AWS_REGION" "$@"
--- iac-localstack/terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "s3_bucket_name" {
-  value = aws_s3_bucket.demo.bucket
-}
-
-output "dynamodb_table_name" {
-  value = aws_dynamodb_table.demo.name
-}
